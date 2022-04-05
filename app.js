6c3ab4066129441887a7cd4b50735051a6766036
ElizabethHolgate <44809627+ElizabethHolgate@users.noreply.github.com>
2022-04-05 12:46:22 +0100

    added create and changed the structure of the app from lecturer to module

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Lecturer = require('./model/lecturer');
+const Lecturer = require('./models/lecturer');
+const Module = require('./models/module');
 
 let url = "mongodb+srv://elizabeth:GA3zRjUwqtXC6U1W@coding-feedback-applica.kwyi9.mongodb.net/codeFeedbackApplication?retryWrites=true&w=majority";
 mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
@@ -17,13 +18,34 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+//app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
-app.get('/lecturer', async(req, res) => {
-    const lecturers = await Lecturer.find({});
-    res.render('lecturer/index', { lecturers })
+app.get('/modules/new', (req, res) => {
+    res.render('modules/new');
 });
+app.post('/modules', async (req, res) => {
+    const module = new Module(req.body.module);
+    await module.save();
+    res.redirect(`/modules/${module._id}`)
+});
+app.get('/modules', async(req, res) => {
+    const modules = await Module.find({});
+    res.render('modules/index', { modules });
+});
+app.get('/lecturers', async(req, res) => {
+    const lecturer = await Lecturer.find({ _id: '624c130e7b5228a4eb7c3fd0'});
+    res.render('lecturers/index', { lecturer })
+});
+
+app.get('/modules/:id', async(req, res) => {
+    const module = await Module.findById(req.params.id);
+    res.render('modules/show', { module });
+});
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')

7fdb6cf521d28b83ece090ad569300795c4a6930
ElizabethHolgate <44809627+ElizabethHolgate@users.noreply.github.com>
2022-04-06 12:16:52 +0100

    added bootstrap and ejs-mate
    added bootstrap and ejs-mate because I couldn't take it anymore!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Lecturer = require('./models/lecturer');
@@ -18,6 +19,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 

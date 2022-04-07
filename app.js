a851621f832400d76f3989d3e81cdf59471ccde5
ElizabethHolgate <44809627+ElizabethHolgate@users.noreply.github.com>
2022-04-07 11:41:22 +0100

    installed session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Lecturer = require('./models/lecturer');
@@ -28,6 +29,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisisasecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
 
 app.use('/modules', modules);
 app.use('/modules/:id/tasks', tasks);
